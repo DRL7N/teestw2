d440e2dad9a71cdbd7a66276907067a609bc463a
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-11-25 21:30:21 +0300

    Create system.js

---
+++ system.js
@@ -0,0 +1,14 @@
+exports.PREFIX = '?'; // امر البوت 
+
+exports.GUILD = '488817662294753306'; // اي دي السيرفر الي يكشف النشر
+exports.BOTID = '491658461545758723'; // اي دي البوت
+exports.USERID = '516309593672908812'; // اي دي حساب النشر
+
+exports.INVITE = '3jrzEEw'; // كود الدعوة الي يدخل عن طريقها الحساب
+exports.CHANNELID = '515947197695524885'; // الشات الي يرسل فية الرابط
+exports.OWNERS = ['383394687349948426','286100702038851585']; // الشات الي يرسل فية الرابط
+
+
+exports.ONLYADVERT = true;
+exports.sinceLastLJ  = 0;
+exports.cachedDMS = []
